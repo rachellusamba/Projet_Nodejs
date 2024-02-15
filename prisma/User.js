99dd238105caca55dd7d5f85f77d406f64039c4f
Rachel <rachellusamba26@gmail.com>
2024-02-15 15:22:29 +0200

    àjour

--- prisma/User.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
- 
- model User {
-       id Int @id @default(autoincrement())
-       email String @unique
-       password String
-     }
