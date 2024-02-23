b68816dbfea22f683f7069e117aa84656c9a1ed1
Rachel <rachellusamba26@gmail.com>
2024-02-23 16:26:53 +0200

    merge develop in main

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ const app = express();
 const { Client } = require('pg');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('./VerifyToken');
-const script = require('./script');
 
 //tableau API
 const users = [
@@ -11,7 +10,7 @@ const users = [
         id: "1",
         profile: "https://www.istockphoto.com/photo/website-page-contact-us-or-e-mail-marketing-concept-customer-support-hotline-contact-gm1477509958-506048670",
         name: "Rachael Lmr",
-        like: "896",
+        password: "1999",
         text: "Even Mexico uses Voter I.D.",
         image: "https://cdn.pixabay.com/photo/2016/03/26/22/33/smartphone-1281632_1280.jpg"
     },
@@ -19,7 +18,7 @@ const users = [
         id: "2",
         profile: "https://cdn.pixabay.com/photo/2015/04/05/08/21/heart-707504_1280.jpg",
         name: "Rachel Lusamba",
-        like: "8",
+        password: "1998",
         text: "When I originally became your all time favorite President, the Great State of Michigan was hemorrhaging car companies and jobs.",
         image: "https://cdn.pixabay.com/photo/2013/11/14/12/34/neckties-210347_1280.jpg"
     },
@@ -27,7 +26,7 @@ const users = [
         id: "3",
         profile: "https://cdn.pixabay.com/photo/2018/05/25/08/35/beauty-3428550_1280.jpg",
         name: "Mpiana Rachael",
-        like: "24785",
+        password: "1997",
         text: "...As I said at the debate – “Will you remember that Texas?Pennsylvania? Ohio? New Mexico?” !",
         image: "https://cdn.pixabay.com/photo/2017/03/19/10/47/friendship-2156172_1280.jpg"
 
@@ -36,7 +35,7 @@ const users = [
         id: "4",
         profile: "https://cdn.pixabay.com/photo/2017/02/23/17/41/illustrator-2092779_1280.jpg",
         name: "Rachel Lusamba",
-        like: "1024",
+        password: "1996",
         text: "...a cheerleader for NAFTA – sending your auto Jobs to Mexico. He voted for China’s entry into the World Trade Organization .",
         image: "https://cdn.pixabay.com/photo/2017/03/19/10/47/friendship-2156171_1280.jpg"
     },
@@ -44,13 +43,12 @@ const users = [
         id: "5",
         profile: "https://cdn.pixabay.com/photo/2016/03/20/17/14/seo-1268989_1280.jpg",
         name: "Lus Mpian R.",
-        like: "85",
+        password: "1995",
         text: "Yvette Herrell (@Yvette4Congress) is a proven fighter for New Mexico! She strongly supports our Brave Law Enforcement, Life and the Second Amendment.",
         image: "https://cdn.pixabay.com/photo/2015/04/05/08/21/pair-707502_1280.jpg"
     }
 ]
-
-const private_key =`-----BEGIN RSA PRIVATE KEY-----
+const private_key = `-----BEGIN RSA PRIVATE KEY-----
 MIIEogIBAAKCAQB1isxgdW0MLvCuTEtj70KJrnrEChLaylDakuhYJLaEciIuIkLy
 1Jv1LAtQeKhrGMGqzKu4nNtd0PtC8rhpBRzWR3eg4Hg9gEE7D4qogBw90TuNGasL
 J0sqCxHrxqlzi1vjap1BR3EMwnBNqPV0VtdjgoMv/1J+nYkz5Tdb30V6yps22f+1
@@ -87,47 +85,56 @@ b6DeenRRpsEj0chuZlZm+nZDPX7a6bYVGWSeZkaFgPEymLs5hdAHiJWM0RwWmLrF
 bCXcnjJvr903tKZZSzSkdIOln3Po5J16EkFJmQ+UCdMxJlMOjsn6RrarTa89Riih
 AgMBAAE=
 -----END PUBLIC KEY-----`;
-app.get('/api/users', (req, res) => {
+
+app.get('/api/users', verifyToken, (req, res) => {
     res.send(users);
 });
-app.get("/:id", (req, res) => {
+app.get("/:id", verifyToken, (req, res) => {
     const id = req.params.id
     res.send(users.filter((e) => {
         return e.id == id
     }))
 })
-// // Ajout dans le tableau
+// Ajout dans le tableau
 app.use(express.json())
-app.post("/post", (req, res) => {
+app.post("/post", verifyToken, (req, res) => {
     users.push(req.body);
     res.send("succes ajout");
 });
 // mettre a jour
-app.put('/put', (req, res) => {
+app.put('/put', verifyToken, (req, res) => {
     const userId = req.params.id;
     res.send(users.filter((e) => {
         return users.splice(userId - 1, 1, req.body)
     }));
 });
 // Supprimer
-app.delete('/:id', (req, res) => {
+app.delete('/:id',verifyToken, (req, res) => {
     const userId = req.params.id;
     users.filter(((e) => {
         return users.splice(userId - 1, 1)
     }))
     res.send("suppression du post effectuée avec succès ")
 })
+
+const secretKey = '12345678';
+const options = {expiresIn: '1 week'};
+//Route pour 
+app.get('/proteger', verifyToken, (req, res) => {
+    res.json({ message: 'Route protégée par JWT', user:req });
+});
+//Route pour générer
+app.post('/login', (req, res) => {     
+    const payload = {
+        utilisateur: 'rachel',
+        role: 'admin'
+    };
+    const token = jwt.sign(payload, secretKey, options);
+    res.json(token);
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/protected-route', verifyToken, (req, res) => {
-    res.json({message:'Route protégée'});
-});
-
-//inserer le token dans la bd
-// INSER INTO utilisateurs(token_authentification) VALUES ('votre_token') WHERE id_utilisateur = 'id_utilisateur';
-
-//recuperation du token
-// SELECT token_authentification FROM utilisateurs WHERE id_utilisateur = 'id_utilisateur';
 
--- crypto.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function generateToken(length) {
-    return crypto.randomBytes(length).toString('hex');
-}
-
-const token = generateToken(32);
-console.log(token);
